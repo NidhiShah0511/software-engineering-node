4e5ae0784b226a435df3ff81b86f14fc412da371
Nidhi Shah <nidhi@Nidhis-MacBook-Pro.local>
2022-11-23 18:03:06 -0500

    Resolving merge conflict

--- server.ts
+++ server.ts
@@ -18,7 +18,7 @@ const session = require("express-session");
 const app = express();
 
 const isProductionEnv = process.env.ENV === 'PRODUCTION';
-//console.log("Current environment ", process.env.ENV, " is production: ", isProductionEnv);
+console.log("Current environment ", process.env.ENV, " is production: ", isProductionEnv);
 app.use(cors({
     credentials: true,
     origin: ['http://localhost:3000','http://localhost', 'https://nshah-tuiter-app-react.netlify.app' ]
